699739e889954e019eb828698351bb47b0eda8cb
Oswaldo Alonzo <oalonzo@gmail.com>
2021-05-28 21:14:00 -0600

    responsive tester sitios web

--- js_exercices/js/index_dom.js
+++ js_exercices/js/index_dom.js
@@ -5,6 +5,7 @@ import countdown from "./cuenta_regresiva.js";
 import scrollTopButton from "./boton_scroll.js";
 import darkTheme from "./tema_oscuro.js";
 import responsiveMedia from "./objeto_responsive.js";
+import responsiveTester from "./prueba_responsive.js";
 
 const d = document;
 
@@ -30,6 +31,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     `<a href="https://goo.gl/maps/Dj1fxAmqm9vUTAXXA" target="_blank" rel="noopener">Ver Mapa</a>`,
     `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.7557477903665!2d-90.51881098464375!3d14.612985280689482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a22d5e112a73%3A0x3a08206ce8a72d8c!2sTorre%20del%20Reformador!5e0!3m2!1sfr!2sgt!4v1622251673876!5m2!1sfr!2sgt" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`
   );
+  responsiveTester("resposive-tester");
 });
 /* keydown - cuando se presiona la tecla
 keyup  - cuando se suelta la tecla
