9944f55990ef040e7599f44b1b99188a277a25bb
Oswaldo Alonzo <oalonzo@gmail.com>
2021-05-30 21:34:57 -0600

    Corrigiendo errores de UserAgent

--- js_exercices/js/index_dom.js
+++ js_exercices/js/index_dom.js
@@ -7,6 +7,7 @@ import darkTheme from "./tema_oscuro.js";
 import responsiveMedia from "./objeto_responsive.js";
 import responsiveTester from "./prueba_responsive.js";
 import userDeviceInfo from "./deteccion_dispositivos.js";
+import networkStatus from "./deteccion_red.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -45,3 +46,4 @@ d.addEventListener("keydown", (e) => {
 });
 
 darkTheme(".dark-theme-btn", "dark-mode");
+networkStatus();
