f55f8c091557d4d14a5c2c2fe586b925c82c1529
Oswaldo Alonzo <oalonzo@gmail.com>
2021-06-01 19:45:41 -0600

    agregando search Filter

--- js_exercices/js/index_dom.js
+++ js_exercices/js/index_dom.js
@@ -10,6 +10,7 @@ import userDeviceInfo from "./deteccion_dispositivos.js";
 import networkStatus from "./deteccion_red.js";
 import webCam from "./deteccion_webcam.js";
 import getGeolocation from "./geolocalizacion.js";
+import searchFilters from "./filtro_busquedas.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -38,6 +39,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
   userDeviceInfo("user-device");
   webCam("webcam");
   getGeolocation("geolocation");
+  searchFilters(".card-filter", ".card");
 });
 
 /* keydown - cuando se presiona la tecla
