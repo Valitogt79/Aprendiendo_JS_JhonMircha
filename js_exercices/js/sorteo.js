3c927b1f53d6b819597084fc30af9a7d129362a3
Oswaldo Alonzo <oalonzo@gmail.com>
2021-06-01 20:31:00 -0600

    agregando archivos de sorteo digital

---
+++ js_exercices/js/sorteo.js
@@ -0,0 +1,20 @@
+const d = document,
+  w = window,
+  n = navigator;
+export default function draw(btn, selector) {
+  const getWinner = (selector) => {
+    const $players = d.querySelectorAll(selector),
+      random = Math.floor(Math.random() * $players.length),
+      winner = $players[random];
+
+    return `El Ganador es : ${winner.textContent}`;
+  };
+
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(btn)) {
+      let result = getWinner(selector);
+      alert(result);
+      console.log(result);
+    }
+  });
+}
