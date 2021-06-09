4335e74fceed2d8131f3dbad0c686317c242076d
Oswaldo Alonzo <oalonzo@gmail.com>
2021-06-09 12:00:35 -0600

    obteniendo los valores de jsonplaceholder

--- js/107_fetch.js
+++ js/107_fetch.js
@@ -5,4 +5,31 @@
     $fragment = document.createDocumentFragment();
 
   fetch("https://jsonplaceholder.typicode.com/Users")
-}();
+    .then((respuesta) => {
+      //console.log(respuesta);
+      return respuesta.ok ? respuesta.json() : Promise.reject(respuesta);
+    })
+
+    .then((json) => {
+      //console.log(json);
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`; //estos datos son obtenidos del API
+        $fragment.appendChild($li);
+      });
+      $fetch.appendChild($fragment);
+    })
+
+    .catch((error) => {
+      //console.log(error);
+      let message = error.statusText || "Ocurrió un Error";
+      $fetch.innerHTML = `Error: ${error.status}: ${message}`;
+    })
+
+    /* Esto es opcional */
+    .finally(() => {
+      console.log(
+        "Esto se ejecutará independientemente del resultado de la Promesa Fetch"
+      );
+    });
+})();
