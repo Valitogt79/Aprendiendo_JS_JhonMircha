df0afd81803b4d8fa8471ed00a6af64bccc5903a
Oswaldo Alonzo <oalonzo@gmail.com>
2021-06-09 10:51:03 -0600

    agregando archivos para trabajar con fetch

---
+++ js/107_fetch.js
@@ -0,0 +1,8 @@
+/** Función anonima autoejecutable */
+/* Instancia */
+(() => {
+  const $fetch = document.getElementById("fetch"),
+    $fragment = document.createDocumentFragment();
+
+  fetch("https://jsonplaceholder.typicode.com/Users")
+}();
