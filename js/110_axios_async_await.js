1857eb70b32c404b06e14cc54a529195983c1f32
Oswaldo Alonzo <oalonzo@gmail.com>
2021-06-10 11:32:10 -0600

    agregando conexion axios

--- js/110_axios_async_await.js
+++ js/110_axios_async_await.js
@@ -2,5 +2,14 @@
   const $axiosAsync = document.getElementById("axios-async"),
     $fragment = document.createDocumentFragment();
 
-  axios.get("https://jsonplaceholder.typicode.com/Users");
+  async function getData() {
+    try {
+      let respuesta = await axios.get("https://jsonplaceholder.typicode.com/Users"),
+        json = await respuesta.data;
+      console.log(respuesta, json);
+    } catch (error) {
+    } finally {
+      console.log("Esto se ejecutará independientemente del try... catch...");
+    }
+  }
 })();
