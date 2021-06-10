e50f656e28baa0fc106d50f62f42dfd9f5aab3ff
Oswaldo Alonzo <oalonzo@gmail.com>
2021-06-10 12:11:55 -0600

    obteniendo los datos directamente de la Api

--- js/110_axios_async_await.js
+++ js/110_axios_async_await.js
@@ -6,10 +6,22 @@
     try {
       let respuesta = await axios.get("https://jsonplaceholder.typicode.com/Users"),
         json = await respuesta.data;
-      console.log(respuesta, json);
+
+      //console.log(respuesta, json);
+
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`; //estos datos son obtenidos del API
+        $fragment.appendChild($li);
+      });
+      $axiosAsync.appendChild($fragment);
     } catch (error) {
+      // console.log(error.response);
+      let message = error.response.statusText || "Ocurrió un error";
+      $axiosAsync.innerHTML = `Error: ${error.response.status}: ${message}`;
     } finally {
       console.log("Esto se ejecutará independientemente del try... catch...");
     }
   }
+  getData();
 })();
